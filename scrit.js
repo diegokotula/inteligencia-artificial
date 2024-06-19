8746bb7d2498755bc5c6015f23f4b26264ec10a5
Diego <108832603+diegokotula@users.noreply.github.com>
2024-06-19 15:13:01 -0300

    1906

--- scrit.js
+++ scrit.js
@@ -6,8 +6,9 @@ const textoResultados = document.querySelector('.texto-resultados');
 const lista = [item1, item2]
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
-        alternativas: ["Alternativa 1", "Alternativa 2"],
+        enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?
+",
+        alternativas: ["Isso é assustador!", "Alternativa 2"],
     },
     {
         enunciado: "Pergunta 2",
@@ -18,6 +19,21 @@ const perguntas = [
         alternativas: ["Alternativa 1", "Alternativa 2"],
     },
 ];
+let atual = 0;
+let perguntaAtual;
+function mostrapergunta(){
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraalternativas();
+}
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternstivas = document. createElement("button");
+        botaoAlternstivas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternstivas);
+    }
+}
+mostrapergunta();
 const lapis = {
    tamanho: 20,
    tipo: 'HB',
