3d5a8c649ee0132192392d18351e22a6a14a7248
Diego <108832603+diegokotula@users.noreply.github.com>
2024-09-12 08:08:09 -0300

    1209

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,6 +1,6 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: " A vitoria do jei's de 2024 foi a melhor sensação da nossa turma",
         alternativas: [
             {
                 texto: "Isso é assustador!",
